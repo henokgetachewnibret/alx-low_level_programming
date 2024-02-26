2e48041d33ed06441465fa068f8a9d1d2257876d
henokgetachewnibret <henokgetachew703@gmail.com>
2024-02-25 22:56:54 -0800

    Done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * Main - Entry point
  *
- * Return Always 0 for sucessful
+ * Return Always 0 (sucess)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	return (0);
 }
