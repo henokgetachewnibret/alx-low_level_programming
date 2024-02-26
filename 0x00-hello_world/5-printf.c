040a47ba9c88e0492cd879f0832d5763e2f47c61
henokgetachewnibret <henokgetachew703@gmail.com>
2024-02-25 23:45:32 -0800

    Done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  *
  * return 0 (Sucess)
  */
-int main (void)
+int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
